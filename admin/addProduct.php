e4045159cc7a9001de37d8f7d42edd8d6b6a9f02
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-05-01 14:22:46 +0200

    Added fields and a button for the products page.

--- admin/addProduct.php
+++ admin/addProduct.php
@@ -62,17 +62,56 @@ if(isset($_POST['saveCategory'])) {
     <div class="col-md-9">
         <div class="row p-5">
             <div class="col-md-12">
+                <h2 class="text-center text-white bg-dark">Add Products</h2>
+            </div>
+            <div class="col-md-12">
                 <form method="post" enctype="multipart/form-data">
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label text-danger" for="imageTitle">Product Name</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control text-center" placeholder="Enter Product Name" name="imageTitle" required id="imageTitle" autocomplete="off">
-                        </div>
+                    <div class="form-outline mb-4 mt-2 w-50 m-auto p-0">
+                        <label class="form-label text-danger" for="imageTitle">Product Name</label>
+                        <input type="text" class="form-control text-center" placeholder="Enter Product Name" name="imageTitle" required id="imageTitle" autocomplete="off">
                     </div>
-                    <div class="form-group row">
-                        <div class="offset-sm-2 col-sm-10">
-                            <button type="submit" class="btn btn-outline-success btn-block" name="submit">Save Product</button>
-                        </div>
+                    <div class="form-outline mb-4 mt-2 w-50 m-auto">
+                        <label class="form-label text-danger" for="productDescription">Product Description</label>
+                        <input type="text" class="form-control text-center" placeholder="Enter Product Description" name="productDescription" required id="productDescription" autocomplete="off">
+                    </div>
+                    <div class="form-outline mb-4 mt-2 w-50 m-auto">
+                        <label class="form-label text-danger" for="productKeyword">Product Keyword</label>
+                        <input type="text" class="form-control text-center" placeholder="Enter Product Keyword" name="productKeyword" required id="productKeyword" autocomplete="off">
+                    </div>
+                    <div class="form-outline mb-4 w-50 m-auto">
+                        <label class="form-label text-danger" for="productKeyword">Category</label>
+                        <select name="productCategory" id="" class="form-control">
+                            <option value="">Select Category</option>
+                            <option value="jeans">Jeans</option>
+                            <option value="shoes">Shoes</option>
+                        </select>
+                    </div>
+                    <div class="form-outline mb-4 w-50 m-auto">
+                        <label class="form-label text-danger" for="productKeyword">Brand</label>
+                        <select name="productCategory" id="" class="form-control">
+                            <option value="">Select Brand</option>
+                            <option value="jeans">Owdi Design</option>
+                            <option value="shoes">Logic Vibes</option>
+                        </select>
+                    </div>
+                    <div class="form-outline mb-4 mt-2 w-50 m-auto">
+                            <label class="form-label text-danger" for="productImage1">Product Image 1</label>
+                            <input type="file" class="form-control text-center" placeholder="Enter Product Keyword" name="productImage1" required id="productKeyword" autocomplete="off">
+                    </div>
+                    <div class="form-outline mb-4 mt-2 w-50 m-auto">
+                            <label class="form-label text-danger" for="productImage2">Product Image 2</label>
+                            <input type="file" class="form-control text-center" placeholder="Enter Product Keyword" name="productImage2"  autocomplete="off">
+                    </div>
+                    <div class="form-outline mb-4 mt-2 w-50 m-auto">
+                            <label class="form-label text-danger" for="productImage3">Product Image 3</label>
+                            <input type="file" class="form-control text-center" placeholder="Enter Product Keyword" name="productImage3"  autocomplete="off">
+                    </div>
+                    <div class="form-outline mb-4 mt-2 w-50 m-auto">
+                            <label class="form-label text-danger" for="productAmount">Product Amount</label>
+                            <input type="text" class="form-control text-center" placeholder="Enter Product Amount" name="productAmount" required id="productAmount" autocomplete="off">
+                    </div>
+                    <div class="form-outline mb-4 mt-2 w-50 m-auto">
+                        <button type="submit" class="btn btn-outline-success btn-block mt-3" name="saveProduct">Save Product</button>
                     </div>
                 </form>
             </div>
